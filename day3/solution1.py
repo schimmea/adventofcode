a11a57fe3b5ef9b19603e03aa5a0c8b413cf76d7
schimmea <56643071+schimmea@users.noreply.github.com>
2022-02-17 02:37:13 +0100

    refactor: simplify day 3 and make it more readable.

--- day3/solution1.py
+++ day3/solution1.py
@@ -1,23 +1,33 @@
 from typing import List, Tuple
-from scipy.stats import mode
 import numpy as np
 
 from load_input_utils import load_input
 
 
 def to_int_array(inp: List[str]) -> np.ndarray:
+    """
+    Splits the binary numbers into integer 0s and 1s and puts them into an array.
+    Useful for calculating column-wise mode.
+    """
     return np.array([list(binary) for binary in inp], dtype=int)
 
 
 def to_binary_str(a: np.ndarray) -> str:
-    if (d := len(a.shape)) > 1:
-        raise ValueError(f"Array needs to be 1D! Got {d}D.")
+    """Turns 1D int array back into a string binary."""
     return "".join(a.astype(str))
+
+
+def binary_column_modes(a: np.ndarray) -> np.ndarray:
+    colsums = a.sum(0)
+    modes = colsums > len(a) / 2
+    ties = colsums == len(a) / 2
+    modes[ties] = 1
+    return modes.astype(int)
 
 
 def calculate_consumption(inp: List[str]) -> int:
     a = to_int_array(inp)
-    modes = mode(a)[0][0]
+    modes = binary_column_modes(a)
     gamma = to_binary_str(modes)
     epsilon = to_binary_str(1 - modes)
     return int(gamma, 2) * int(epsilon, 2)
--- day3/solution2.py
+++ day3/solution2.py
@@ -1,35 +1,26 @@
 import numpy as np
 
 from load_input_utils import load_input
-from day3.solution1 import to_int_array, to_binary_str
+from day3.solution1 import to_int_array, to_binary_str, binary_column_modes
 
 
-def get_meta_mask(a: np.ndarray, inverse=False, tie_value=0) -> np.ndarray:
-    threshold = len(a) / 2
-    if (col_sum := a.sum()) > threshold:
-        mode = 1 if not inverse else 0
-    elif col_sum < threshold:
-        mode = 0 if not inverse else 1
-    else:
-        mode = tie_value
-    return np.where(a == mode)
-    
-
-def iterative_mode_match(a: np.ndarray, inverse=False, tie_value=0) -> str:
-    mask = np.arange(len(a))
-    position = 0
-    while len(mask) > 1:
-        meta_mask = get_meta_mask(a[mask, position], inverse=inverse, tie_value=tie_value)
-        mask = mask[meta_mask]
-        position += 1
-    solution = a[mask].flatten()
-    return to_binary_str(solution)
+def iterative_mode_match(a: np.ndarray, invert=False) -> str:
+    """Iterate over all bit positions and determine column-wise mode. Drop all numbers that are not mode."""
+    out = a.copy()
+    for position in range(out.shape[1]):
+        modes = binary_column_modes(out)
+        modes = 1 - modes if invert else modes
+        mask = out[:, position] == modes[position]
+        out = out[mask]
+        if len(out) == 1:
+            break
+    return to_binary_str(out[0])
 
 
 def calculate_life_support(inp: list) -> int:
     a = to_int_array(inp)
-    oxygen = iterative_mode_match(a, inverse=False, tie_value=1)
-    co2 = iterative_mode_match(a, inverse=True, tie_value=0)
+    oxygen = iterative_mode_match(a)
+    co2 = iterative_mode_match(a, invert=True)
     return int(oxygen, 2) * int(co2, 2)
 
 
